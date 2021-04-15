2e962fecbc42d64048ddb72aa8e75f3b448e60bf
Abeeb <abeebayinla@gmail.com>
2021-04-15 14:03:55 +0100

    Made changes

--- config/config.js
+++ config/config.js
@@ -3,5 +3,4 @@ require('dotenv').config();
 module.exports = {
     clientId: process.env.CLIENTID,
     secret: process.env.SECRET,
-    callback: 'http://localhost:8888/callback',
 };
--- server.js
+++ server.js
@@ -1,21 +1,58 @@
 const express = require('express');
+const session = require('express-session');
 const passport = require('passport');
-const googleStrategy = require('./config/googleStrategy');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config/config');
 
 // Start express
 const app = express();
 
-app.use(googleStrategy);
+// app.set('view engine', 'ejs');
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
+}));
+
+let userProfile;
+
 app.use(passport.initialize());
+app.use(passport.session());
 
-// Authenticate with google
-app.get('/', passport.authenticate('google', { scope: ['email', 'profile'] }), (req, res) => {
-    console.log('Processing authentication');
+app.set('view engine', 'ejs');
+
+passport.use(new GoogleStrategy({
+    clientID: config.clientId,
+    clientSecret: config.secret,
+    callbackURL: "http://localhost:8888/callback"
+  }, (accessToken, refreshToken, profile, done) => {
+      userProfile = profile;
+      console.log(userProfile);
+      return done(null, userProfile);
+  }
+));
+
+passport.serializeUser((user, cb) => {
+    cb(null, user);
+});
+  
+passport.deserializeUser((obj, cb) => {
+    cb(null, obj);
 });
 
-app.get('/callback', passport.authenticate('google', { scope: ['email', 'profile'] }), (req, res) => {
-    return res.send('Congrats');
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
+ 
+app.get('/', passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/callback', passport.authenticate('google', { failureRedirect: '/error' }), (req, res) => {
+    res.redirect('/success');
 });
+
+// app.get('/', (req, res) => {
+//     res.render('pages/auth');
+// });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
